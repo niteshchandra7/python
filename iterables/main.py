aad236cb83fc6e0bbfda393670b9fc3bcdfc10fd
niteshchandra7 <nitesh240295@gmail.com>
2023-10-01 17:14:23 +0530

    iterator and iterables

---
+++ iterables/main.py
@@ -0,0 +1,36 @@
+class Cars:
+    def __init__(self,car_names:list) -> None:
+        self.cars_names:list = car_names
+        self.index = -1
+    
+    # __next__ returns a value
+    def __next__(self)->str:
+        self.index +=1
+        if self.index >= len(self.cars_names):
+            raise StopIteration
+        else:
+            return self.cars_names[self.index]
+    def __iter__(self):
+        return self
+
+class CarIterables:
+    
+    def __init__(self,car_iterators:Cars) -> None:
+        self.car_iter = car_iterators
+    
+    # __iter__ implements object of an iterator class 
+    def __iter__(self):
+        return self.car_iter
+
+car_names = ["AVC","HYUNDAI","MG"]
+cars = Cars(car_names)
+print(list(cars))
+print(cars.index)
+# print(next(cars))
+# print(next(cars))
+# print(next(cars))
+# print(next(cars))
+
+# car_iterables = CarIterables(cars)
+for car in cars:
+    print(car)
