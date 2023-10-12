cffece0d3316b5e7b35602da3a8d6f55cc5cc262
niteshchandra7 <nitesh240295@gmail.com>
2023-10-12 16:24:26 +0530

    enums

---
+++ enums/main.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+class CarType(Enum):
+    HATCHBACK=0
+    SEDAN=1
+    SUV=2
+    SPORTSCAR=3
+    
+
+class Car:
+    def __init__(self,car_type:CarType,id:int) -> None:
+        self.car_type = car_type
+        self.id=id
+    
+    def __str__(self):
+        return f"Car type for {self.id} car object is {self.car_type}"
+    
+    def __and__(self,obj):
+        return self.id & obj.id  
+
+car1 = Car(CarType.HATCHBACK,1)
+print(car1)
+
+car2 = Car(CarType.SPORTSCAR,2)
+print(car2)
+
+print(car1 & car2)
+print(car1 | car2)
+
+
+
+# if car1.car_type == CarType.HATCHBACK:
+#     print("hatch type car object")
+        
+        
